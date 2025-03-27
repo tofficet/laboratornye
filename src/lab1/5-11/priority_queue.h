9bd8646f4dd441c3c8995318fc664f008f17740d
tofficet <tofficet@student.21-school.ru>
2025-03-27 16:10:08 +0300

    fix

--- src/lab1/5-11/priority_queue.h
+++ src/lab1/5-11/priority_queue.h
@@ -1,25 +1,15 @@
 #ifndef PRIORITY_QUEUE_H
 #define PRIORITY_QUEUE_H
 
-struct Node;  // Предварительное объявление
+struct Node;  
 
 class PriorityQueue {
-private:
-    Node* head;
-    
-    char* copyString(const char* src);
-
 public:
-    PriorityQueue();
-    ~PriorityQueue();
-    
-    void insert(int prior, const char* value);
-    const char* findMax() const;
-    char* deleteMax();
-    PriorityQueue& merge(PriorityQueue& other);
-    
-    PriorityQueue(const PriorityQueue&) = delete;
-    PriorityQueue& operator=(const PriorityQueue&) = delete;
+    virtual ~PriorityQueue();
+    virtual void insert(int prior, const char* value);
+    virtual const char* findMax() const;
+    virtual char* deleteMax();
+    virtual PriorityQueue& merge(PriorityQueue* other);
 };
 
 #endif // PRIORITY_QUEUE_H
