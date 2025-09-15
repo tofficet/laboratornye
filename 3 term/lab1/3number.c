df8d6477508f5b95636b9db51894a9da4a91d94b
tofficet <tofficet@student.21-school.ru>
2025-09-15 23:00:49 +0300

    3number

--- 3 term/lab1/3number.c
+++ 3 term/lab1/3number.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
-int main(){
-    
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        printf("%s", argv[0]);
+        return 1;
+    }
+
+    char *filename1 = argv[1];
+    char *filename2 = argv[2];
+    char buffer[1024];
+    size_t bytes_read;
+
+    FILE *f1 = fopen(filename1, "rb");   
+    if (!f1) {
+        return 1;
+    }
+
+    FILE *f2 = fopen(filename2, "wb"); 
+    if (!f2) {
+        fclose(f1);
+        return 1;
+    }
+
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), f1)) > 0) {
+        size_t bytes_written = fwrite(buffer, 1, bytes_read, f2);
+        if (bytes_written != bytes_read) {
+            fclose(f1);
+            fclose(f2);
+            return 1;
+        }
+    }
+
+    fclose(f1);
+    fclose(f2);
+
     return 0;
 }
